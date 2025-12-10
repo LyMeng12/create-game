e8a87c7ee3c2fce1bba411c051fedd0a7e3bae52
LyMeng12 <mengsaoly@gmail.com>
2025-12-10 23:06:14 +0700

    Random already

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,42 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+
+    static String[] item(){
+        String[] items = {"🍎","🍊","🍓","🍒","⭐"};
+        Random random = new Random();
+        String[] Row=new String[3];
+
+        for(int i=0;i<3;i++){
+            Row[i]=items[random.nextInt(items.length)];
+        }
+        for(int i=0;i<3;i++){
+            System.out.print("| "+Row[i]);
+        }
+        System.out.print(" |");
+        return items;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("How Many$:");
+        int meny = in.nextInt();
+
+        boolean play = true ;
+        while (play){
+            System.out.println("Current balance:"+meny);
+            System.out.print("Place Your amount:");
+            int place = in.nextInt();
+            System.out.println("Spinning...");
+            System.out.println("*****************");
+            System.out.println("🍎 🍊 🍓 🍒 ⭐");
+            item();
+
+        }
+
     }
 }
